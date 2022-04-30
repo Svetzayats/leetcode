4e84ec762ac9bd8cd3e2cca0a9fb550e0a5dee2c
Svetlana <svetzayats@gmail.com>
2022-04-30 21:45:26 +0500

    Update IntersectionOfTwoArrays.js

--- IntersectionOfTwoArrays.js
+++ IntersectionOfTwoArrays.js
@@ -1,3 +1,10 @@
+/**
+ * Given two integer arrays nums1 and nums2,
+ * return an array of their intersection.
+ * Each element in the result must appear as many times as it shows in both arrays
+ * and you may return the result in any order.
+ */
+
 // simple solution
 const intersect = function (nums1, nums2) {
     const baseArray = nums1.length <= nums2.length ? nums1 : nums2;
