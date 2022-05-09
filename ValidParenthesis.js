4d81ccc14a01f99b410bbb5056539f0cd1941ea8
Svetlana <svetzayats@gmail.com>
2022-05-09 13:37:19 +0500

    Create ValidParenthesis.js

---
+++ ValidParenthesis.js
@@ -0,0 +1,32 @@
+/**
+ * Given a string s containing just the characters '(', ')', '{', '}', '[' and ']',
+ * determine if the input string is valid.
+ * An input string is valid if:
+ * Open brackets must be closed by the same type of brackets.
+ * Open brackets must be closed in the correct order.
+ */
+
+function isValid(s) {
+    if (s.length % 2 !== 0) return false;
+
+    const brackets = {
+        '(': ')',
+        '[': ']',
+        '{': '}'
+    };
+    const stack = [];
+
+    for (let i = 0; i < s.length; i++) {
+        if (brackets[s[i]]) {
+            stack.push(s[i]);
+        } else {
+            if (brackets[stack.pop()] !== s[i]) {
+                return false;
+            }
+        }
+    }
+
+    return !stack.length;
+}
+
+console.log(isValid('()'));
